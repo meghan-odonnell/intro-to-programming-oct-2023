0d22a7b894fcd8c4949d1ade0ed95d885f1e3e75
Meghan O'Donnell <odonnell.meghan@gmail.com>
2023-10-24 17:02:33 -0400

    tuesday

---
+++ stringcalculator-master/StringCalculator.cs
@@ -0,0 +1,23 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        int sum;
+        if (numbers.Length == 0)
+        {
+            return 0;
+        }
+
+        foreach (string number in numbers)
+        {
+            sum = int.Parse(number);
+
+
+        }
+
+    }
+}
---
+++ stringcalculator-master/StringCalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿
+namespace StringCalculator;
+
+
+public class StringCalculatorTests
+{
+    //  [Theory]
+    //  [InlineData("")]
+    //  [InlineData("1")]
+    //  [InlineData("1,2")]
+
+
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+
+    [Fact]
+    public void OneInputReturnsThatNumber()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("1");
+        Assert.Equal(1, result);
+
+    }
+}
